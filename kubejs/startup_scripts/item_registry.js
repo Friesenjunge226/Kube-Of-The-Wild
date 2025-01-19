82df9468d8bdcbd73e554da42602e7dce0870f49
Friesenjunge226 <131612004+Friesenjunge226@users.noreply.github.com>
2025-01-19 10:41:25 +0100

    Aktualisieren von item_registry.js
    Added Tier registration
    Added Item registry

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,1 +1,28 @@
+ItemEvents.toolTierRegistry(event => {
+  event.add('Admin', tier => {
+    tier.uses = 99999
+    tier.speed = 0.1
+    tier.attackDamageBonus = 99999.0
+    tier.level = 99999
+    tier.enchantmentValue = 0
+  })
+})
 
+ItemEvents.armorTierRegistry(event => {
+  event.add('Admin', tier => {
+    tier.durabilityMultiplier = 99999 // Each slot will be multiplied with [13, 15, 16, 11]
+    tier.slotProtections = [999, 999, 999, 999] // Slot indicies are [FEET, LEGS, BODY, HEAD] 
+    tier.enchantmentValue = 99999
+    tier.equipSound = 'minecraft:item.armor.equip_nerherite'
+    tier.repairIngredient = 'minecraft:air'
+    tier.toughness = 99999.0 // diamond has 2.0, netherite 3.0
+    tier.knockbackResistance = 99999.0
+  })
+})
+
+
+StartupEvents.registry('item', e => {
+
+  e.create('Master_Sword', 'sword').attackDamageBaseline(10.0).maxDamage(191).displayName("Master Sword").glowing(true).unstackable().rarity(rare)
+
+})
