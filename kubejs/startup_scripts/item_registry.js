ca1b7f9dade470d537a1e002cf7d64813310e940
Friesenjunge226 <131612004+Friesenjunge226@users.noreply.github.com>
2025-01-25 13:32:00 +0000

    Added Stuff

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -23,22 +23,28 @@ ItemEvents.armorTierRegistry(event => {
 
 
 StartupEvents.registry('item', e => {
-  //Admin tools and weapons
-  e.create('Ban_Hammer').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Sword', 'sword').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Axe', 'axe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Pickaxe', 'pickaxe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Shovel', 'shovel').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Hoe', 'hoe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  //Admin Armor
-  e.create('Admin_Helmet', 'helmet').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Chestplate', 'chestplate').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Leggings', 'leggings').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-  e.create('Admin_Boots', 'boots').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+  //Admin tools
+   e.create('ban_hammer').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_sword', 'sword').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_axe', 'axe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_pickaxe', 'pickaxe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_shovel', 'shovel').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_hoe', 'hoe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+  //admin armor
+   e.create('admin_helmet', 'helmet').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_chestplate', 'chestplate').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_leggings', 'leggings').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_boots', 'boots').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+  //comunity weapons
+   e.create('master_sword', 'sword').attackDamageBaseline(30.0).maxDamage(20).displayName("Master Sword").glowing(true).unstackable().rarity(rare).tooltip("The Legendary sword that seals the darkness. It's blade gleams with a sacred luster that can oppose the Calamity. Only a hero chosen by the sword itself may wield it.")
+   e.create('tree_branch', 'sword').displayName("Tree Branch").attackDamageBaseline(2.0).maxDamage(4).unstackable().rarity('common').tooltip("Wooden branches such as this are pretty common, but it's urprisingly well-balanced. It doesn't do much damage but can serve as a weapon in a pinch.")
+   e.create('traveller_sword', 'sword').displayName("Traveller's Sword").attackDamageBaseline(5.0).maxDamage(20).unstackable().rarity('common').tooltip("A very common sword often kept by travelers to fend off small beasts. It's fairly durable, but a bit unreliable against monsters.")
+   e.create('soldier_broadsword','sword').displayName("Soldier's Broadsword").attackDamageBaseline(14.0).maxDamage(25).unstackable().rarity('common').tooltip("A Sword brandished by the soldiers who once protected Hyrule Castle. Its durable blade is well tuned for slaying monsters.")
+   e.create('')
 
-  //community weapons
-  e.create('Master_Sword', 'sword').attackDamageBaseline(30.0).maxDamage(191).displayName("Master Sword").glowing(true).unstackable().rarity(rare)
-  e.create('Travellers')
-  //food
+
+
+
+
 
 })
