86a57e778efb2dea6399295abcbcaea4d8de6835
Friesenjunge226 <131612004+Friesenjunge226@users.noreply.github.com>
2025-02-07 09:17:12 +0000

    Aktualisieren von de_de.json Localised Administrative Tools

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -24,17 +24,17 @@ ItemEvents.armorTierRegistry(event => {
 
 StartupEvents.registry('item', e => {
   //Admin tools
-   e.create('ban_hammer').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_sword', 'sword').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_axe', 'axe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_pickaxe', 'pickaxe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_shovel', 'shovel').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_hoe', 'hoe').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('ban_hammer').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_sword', 'sword').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_axe', 'axe').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_pickaxe', 'pickaxe').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_shovel', 'shovel').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_hoe', 'hoe').tier('Admin').unstackable().rarity('rare').tooltip("Node, not that kind of hoe")
   //admin armor
-   e.create('admin_helmet', 'helmet').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_chestplate', 'chestplate').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_leggings', 'leggings').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
-   e.create('admin_boots', 'boots').tier('Admin').unstackable().rarity('rare').tooltip(oooh. Shiny)
+   e.create('admin_helmet', 'helmet').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_chestplate', 'chestplate').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_leggings', 'leggings').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
+   e.create('admin_boots', 'boots').tier('Admin').unstackable().rarity('rare').tooltip("oooh. Shiny")
   //comunity weapons
    e.create('master_sword', 'sword').attackDamageBaseline(30.0).maxDamage(20).displayName("Master Sword").glowing(true).unstackable().rarity('rare').tooltip("The Legendary sword that seals the darkness. It's blade gleams with a sacred luster that can oppose the Calamity. Only a hero chosen by the sword itself may wield it.")
    e.create('tree_sword', 'sword').displayName("Tree Branch").attackDamageBaseline(2.0).maxDamage(4).unstackable().rarity('common').tooltip("Wooden branches such as this are pretty common, but it's urprisingly well-balanced. It doesn't do much damage but can serve as a weapon in a pinch.")
