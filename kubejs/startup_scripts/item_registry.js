968569cd41807661497e30d354a0a8d97f1d0e2b
Friesenjunge226 <131612004+Friesenjunge226@users.noreply.github.com>
2025-02-21 09:51:44 +0000

    Added Item_registry.js

---
+++ kubejs/startup_scripts/item_registry.js
@@ -0,0 +1,40 @@
+//priority: 1
+
+
+
+
+//key items
+
+e.create('classified_envelope').displayName("Classified Envelope").rarity('common').tooltip("Inside the envelope is a picture of a beautiful young woman of the Sheikah tribe.")
+e.create('rudania_power').displayName("Daruk's Protection").rarity('common').tooltip("")
+
+  //DLC
+  e.create('dlc_rudania_power_plus').displayName("").rarity('common').tooltip("")
+  e.create('dlc_ancient_bridle').displayName("Ancient Bridle").rarity('common').tooltip("A mysterious bridle made with forgotten ancient technology. Equipping a horse with this will maximize their abilities, increasing the number of spurs associated with them.")
+  e.create('dlc_ancient_saddle').displayName("Ancient Saddle").rarity('common').tooltip("A mysterious saddle made with forgotten ancient technology. Equip a horse with this and use D-Pad Down to call them even when they are far away.")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+
+
+  e.create('').displayName("").rarity('common').tooltip("")
+
+  //"gifts"
+
+  e.create('gift_hestu').displayName("Hestu's Gift").rarity('common').tooltip("")
