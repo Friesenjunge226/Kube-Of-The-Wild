7519fbcf4f0ee62b1f7b0b1fda4cdc2e92e611f4
Friesenjunge226 <131612004+Friesenjunge226@users.noreply.github.com>
2025-02-24 09:15:08 +0100

    0.4.0
    * Aktualisieren von item_registry.js
    
    * Delete kubejs/assets/Lol.jpeg
    
    * Edit startup_sripts.js
    Added Admin
    
    * Aktualisieren von item_registry.js
    Added Weapons
    
    * Added Stuff
    
    * Changed item_registry.js
    Added till item 227
    
    * Aktualisieren von item_registry.js
    
    Added Softener
    
    * Aktualisieren von item_registry.js
    Added till Double Axe
    
    * Aktualisieren von item_registry.js
    Added items 198-277
    
    * Aktualisieren von item_registry.js
    Added Items 198-300
    
    * Aktualisteren von item_registry.js
    Added items 300–303
    
    * Aktualisieren von item_registry.js
    Added more weapons (Almost done)
    
    * Aktualisieren von Sprachpaketen
    Added de_de
     -Added language (NOT APPROVED)
    
    * Aktualisieren von de_de.json
    Localised Administrative Tools
    
    * Changing README.md
    Added first pat of installation.
    
    * Aktualisieren von README.md
    
    Added final text.
    
    * Aktualisieren von README.md
    
    Added License banner.
    (Probably wrong tho)
    
    * Aktualisieren von README.md
    
    Still wrong probably
    
    * Aktualisieren von README.md
    
    Reverted change 3b50db50b4a684a3d9c7a81b48f99c74de72b903 and 3565989a08e2f314816665b59614415830509a2e
    
    * Aktualisieren von README.md
    
    * Update de_de.json
    
    Added Localisation
    
    * Changing item_registry.js
    Debugged code
    
    * Aktualisieren von README.md
    
    Added disclaimer
    
    * Update de_de.json
    
    So I can say I did something...
    
    * Update Crowdin configuration file
    
    * Aktualisieren von README.md
    
    Added widgets
    
    * Aktualisieren von README.md
    
    * Aktualisieren von README.md
    
    * Aktualisieren von README.md
    
    * Aktualisieren von README.md
    
    Added Banner
    
    * Update item_registry.js
    
    Corrected Typo
    
    * Create en_us.json
    
    Added Loacalisation template
    
    * New translations item_registry.js (French)
    
    * New translations item_registry.js (Spanish)
    
    * New translations item_registry.js (Danish)
    
    * New translations item_registry.js (German)
    
    * New translations item_registry.js (Portuguese)
    
    * New translations item_registry.js (Russian)
    
    * New translations item_registry.js (Ukrainian)
    
    * New translations item_registry.js (Chinese Simplified)
    
    * New translations item_registry.js (Chinese Traditional)
    
    * New translations item_registry.js (English)
    
    * New translations item_registry.js (English (upside down))
    
    * Update Crowdin configuration file
    
    * Update README.md
    
    * Delete kubejs/assets/kubejs/textures/item/test_item.png
    
    * Delete kubejs/da_DK.json
    
    * Delete kubejs/de_DE.json
    
    * Delete kubejs/en_UD.json
    
    * Delete kubejs/en_US.json
    
    * Delete kubejs/es_ES.json
    
    * Delete kubejs/fr_FR.json
    
    * Delete kubejs/pt_PT.json
    
    * Delete kubejs/ru_RU.json
    
    * Delete kubejs/uk_UA.json
    
    * Delete kubejs/zh_CN.json
    
    * Delete kubejs/zh_TW.json
    
    * Update item_registry.js
    
    Added Powerless and Glowing Variants
    
    * Update de_de.json
    
    * Aktualisieren von README.md
    
    Rephrased
    
    * Delete crowdin.yml
    
    * Aktualisieren von item_registry.js
    
    Added unnecessary stuff
    
    * Aktualisieren von de_de.json
    
    Hinzufügen von unnötigen Dingen
    
    * Update README.md
    
    Added Image
    
    * Update README.md
    
    Added better Image
    
    * Update README.md
    
    * Changed name
    
    * Deleted iem_registry.js
    
    * Added item_registry.js
    Added key Items
    
    * Added Item_registry.js
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
+![Badge](https://img.shields.io/badge/version-v_0.3.3-blue) [![GitHub issues](https://img.shields.io/github/issues-raw/Friesenjunge226/Kube-of-the-wild)](https://github.com/users/Friesenjunge226/projects/2)[![GitHub all releases](https://img.shields.io/github/downloads/Friesenjunge226/Kube-of-the-wild/total)](https://github.com/BlueMap-Minecraft/BlueMap/releases)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/Friesenjunge226/Kube-of-the-wild)](https://github.com/BlueMap-Minecraft/BlueMap/releases/latest)
+
+[![Discord Banner 4](https://discordapp.com/api/guilds/1225836171762860113/widget.png?style=banner2)](https://discord.com/invite/b77hdjUuyX)
 # Kube of the Wild
 
-> This Project is _Very_ much work in progress and just has the weapons barely done(experimental). Please wait patiently, I'm working on it
+> This Project is _Very_ much work in progress and just has the weapons barely done. Please wait patiently, I'm working on it
 
 **Welcome to the Kube of the Wild Project** 
-This Project is a kind-of-Addon to the [The Legend of Zelda® in Minecraft® Project](https://m.youtube.com/watch?v=lmdl2Wu7PO0) of the Youtuber [Grazzy](https://www.youtube.com/grazzyy), but you still can play it witout the Map. This is a free project, and I do not plan on making any Money with it.
+This Project is a kind-of-Addon to the [The Legend of Zelda® in Minecraft® Project](https://m.youtube.com/watch?v=lmdl2Wu7PO0) of the Youtuber [Grazzy](www.youtube.com/grazzyy). This is a free project, and I do not plan on making Money with it.
 
 I recommend you use a Mod Manager like [Curseforge](https://www.curseforge.com/download/app) or [Modrinth](https://modrinth.com/app) for this.
+
+> This is the experimental Branch, which will not always work. I'm commiting to this branch all the time and if it's ready, I release it to the main branch. Pleases respect that.
 
 Anyways, have fun!
 
@@ -23,11 +29,9 @@ Download the Kube of the Wild Project in .zip from this Github, unzip it and put
 
 >To open the Folder, click Options (Three dots) and then open folder.
 
-The folder structure should look like this now: 
+The folder structure should now roughly look like this: 
 
-"Put image of folder here."
+![image](https://github.com/user-attachments/assets/9909dbc3-ca0d-4427-a311-41dd1275a5c2)
 
 Congratulations, you are ready to start Minecraft.
-If you encounter __any__ Errors with KubeJS you can make an Issue, to let me know.
-
-Please don't sue me. 
+If you encounter __any__ Errors with KubeJS you can make an Issue, to let me know. 
--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,28 +1,40 @@
-ItemEvents.toolTierRegistry(event => {
-  event.add('Admin', tier => {
-    tier.uses = 99999
-    tier.speed = 0.1
-    tier.attackDamageBonus = 99999.0
-    tier.level = 99999
-    tier.enchantmentValue = 0
-  })
-})
-
-ItemEvents.armorTierRegistry(event => {
-  event.add('Admin', tier => {
-    tier.durabilityMultiplier = 99999 // Each slot will be multiplied with [13, 15, 16, 11]
-    tier.slotProtections = [999, 999, 999, 999] // Slot indicies are [FEET, LEGS, BODY, HEAD] 
-    tier.enchantmentValue = 99999
-    tier.equipSound = 'minecraft:item.armor.equip_nerherite'
-    tier.repairIngredient = 'minecraft:air'
-    tier.toughness = 99999.0 // diamond has 2.0, netherite 3.0
-    tier.knockbackResistance = 99999.0
-  })
-})
+//priority: 1
 
 
-StartupEvents.registry('item', e => {
 
-  e.create('Master_Sword', 'sword').attackDamageBaseline(10.0).maxDamage(191).displayName("Master Sword").glowing(true).unstackable().rarity(rare)
 
-})
+//key items
+
+e.create('classified_envelope').displayName("Classified Envelope").rarity('common').tooltip("Inside the envelope is a picture of a beautiful young woman of the Sheikah tribe.")
+e.create('rudania_power').displayName("Daruk's Protection").rarity('common').tooltip("")
+
+  //DLC
+  e.create('dlc_rudania_power_plus').displayName("").rarity('common').tooltip("")
+  e.create('dlc_ancient_bridle').displayName("Ancient Bridle").rarity('common').tooltip("A mysterious bridle made with forgotten ancient technology. Equipping a horse with this will maximize their abilities, increasing the number of spurs associated with them.")
+  e.create('dlc_ancient_saddle').displayName("Ancient Saddle").rarity('common').tooltip("A mysterious saddle made with forgotten ancient technology. Equip a horse with this and use D-Pad Down to call them even when they are far away.")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+  e.create('').displayName("").rarity('common').tooltip("")
+
+
+  e.create('').displayName("").rarity('common').tooltip("")
+
+  //"gifts"
+
+  e.create('gift_hestu').displayName("Hestu's Gift").rarity('common').tooltip("")
